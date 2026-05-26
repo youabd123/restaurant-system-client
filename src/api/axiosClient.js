e5ce04c0bfc1e3bd41f531ace17ba6380d201492
yonisb77 <yonis_77@hotmail.com>
2026-05-26 14:30:21 +0200

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'https://localhost:7134/api',
+    baseURL: 'http://localhost:5037/api',
     headers: {
         'Content-Type': 'application/json',
     },
